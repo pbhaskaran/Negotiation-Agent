8b7e7c24847d2416ca4fd822aee3be9048d07347
Bram Renting <bramrenting@gmail.com>
2022-01-28 20:36:34 +0000

    added comments

--- run.py
+++ run.py
@@ -1,10 +1,20 @@
 from utils.create_runner import create_runner
 
+# Settings to run a negotiation session:
+#   We need to specify the classpath of 2 agents to start a negotiation.
+#   We need to specify one of the domains (name of one of the directories in the ./domains directory)
+#   We need to specify a deadline of amount of rounds we can negotiate before we end without agreement
 settings = {
-    "agents": ["agents.random_agent.random_agent.RandomAgent", "agents.template_agent.template_agent.TemplateAgent"],
+    "agents": [
+        "agents.random_agent.random_agent.RandomAgent",
+        "agents.template_agent.template_agent.TemplateAgent",
+    ],
     "domain": "jobs",
     "deadline_rounds": 200,
 }
 
+# obtain the negotiation session runner
 runner = create_runner(settings)
+
+# run the session
 runner.run()
