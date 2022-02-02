d5f9634148d0520d99124358772087a6d09e34ad
Bram Renting <bramrenting@gmail.com>
2022-02-02 17:24:35 +0000

    added result parsing after run

--- run.py
+++ run.py
@@ -1,20 +1,28 @@
-from utils.create_runner import create_runner
+import json
+import os
+
+from utils.plot_trace import plot_trace
+from utils.run_session import run_session
+
+# create results directory if it does not exist
+if not os.path.exists("results"):
+    os.mkdir("results")
 
 # Settings to run a negotiation session:
 #   We need to specify the classpath of 2 agents to start a negotiation.
-#   We need to specify one of the domains (name of one of the directories in the ./domains directory)
+#   We need to specify the preference profiles for both agents. The first profile will be assigned to the first agent.
 #   We need to specify a deadline of amount of rounds we can negotiate before we end without agreement
 settings = {
     "agents": [
         "agents.random_agent.random_agent.RandomAgent",
         "agents.template_agent.template_agent.TemplateAgent",
     ],
-    "domain": "jobs",
+    "profiles": ["domains/jobs/jobsprofileA.json", "domains/jobs/jobsprofileB.json"],
     "deadline_rounds": 200,
 }
 
-# obtain the negotiation session runner
-runner = create_runner(settings)
+# run a session and obtain results in dictionaries
+results_trace, results_summary = run_session(settings)
 
 # run the session
 runner.run()
